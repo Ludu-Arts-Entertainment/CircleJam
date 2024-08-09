055636609ff0044202d370a56180564a0bc11e5a
Aleyna <aleyna@luduarts.com>
2024-08-09 17:11:43 +0300

    Level creator only work in editor

--- Assets/_Game/Scripts/LevelEditor/LevelCreation/LevelCreator.cs
+++ Assets/_Game/Scripts/LevelEditor/LevelCreation/LevelCreator.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -90,3 +91,4 @@ public class LevelCreator : MonoBehaviour
         }
     }
 }
+#endif
